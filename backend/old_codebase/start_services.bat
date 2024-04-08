78d44939b23e82c81c5e4a9ea9a16a6dbc014f73
Redwanul Islam <44495931+redwanine@users.noreply.github.com>
2024-04-07 23:16:43 -0600

    Delete Outdated Content

--- backend/old_codebase/start_services.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-start cmd /k "set SERVICE_ID=1 && python C:\CPSC_559_Project\communication.py"
-start cmd /k "set SERVICE_ID=2 && python C:\CPSC_559_Project\communication.py"
-start cmd /k "set SERVICE_ID=3 && python C:\CPSC_559_Project\communication.py"
-
-rem Start order service instances
-start cmd /k "python order.py"
